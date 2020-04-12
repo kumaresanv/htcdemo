9d62886eababa24ac813fa6b0816e2477649b8a5
kumaresanv <kumaresanvpng@gmail.com>
2020-04-12 13:00:58 -0400

    test1

--- htcdemo/src/test/java/com/htc/htcdemo/testcases/SeleniumEasyHome.java
+++ htcdemo/src/test/java/com/htc/htcdemo/testcases/SeleniumEasyHome.java
@@ -15,6 +15,7 @@ import com.htc.htcdemo.pages.SeleniumEasyHomePage;
 
 public class SeleniumEasyHome extends Base {
 	//Launch the selenium easy demo page and verify selenium easy demo page title
+	//test
 	@Test(dataProvider = "Democomponents", dataProviderClass = DemoMyDataProvider.class)
 	public void TC01_VerifyPageTitle(Map<String, String> testData, Method method) throws MyException {
 		setTestCase(getParentTestCase().createNode(method.getName()));
